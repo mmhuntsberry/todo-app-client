73c8bb5eb1742bef9fb7bb15e2f1cd559c98c907
Matt Huntsberry <matt.huntsberry@nielsen.com>
2022-11-19 09:53:29 -0600

    chore: add signup link

--- src/Signin.js
+++ src/Signin.js
@@ -1,6 +1,6 @@
 import axios from "axios";
 import React, { useState } from "react";
-import { useNavigate } from "react-router-dom";
+import { Link, useNavigate } from "react-router-dom";
 
 export const Signin = (props) => {
   const [email, setEmail] = useState("");
@@ -8,50 +8,55 @@ export const Signin = (props) => {
   const navigate = useNavigate();
 
   return (
-    <form
-      className="form"
-      onSubmit={(e) => {
-        e.preventDefault();
-        axios
-          .post(
-            "https://todo-app-server-tan.vercel.app/signin",
-            {
-              email,
-              password,
-            },
-            {}
-          )
-          .then(function (response) {
-            props.setToken(response.data.token);
-          })
-          .catch(function (error) {
-            console.log(error);
-          });
+    <>
+      <form
+        className="form"
+        onSubmit={(e) => {
+          e.preventDefault();
+          axios
+            .post(
+              "https://todo-app-server-tan.vercel.app/signin",
+              {
+                email,
+                password,
+              },
+              {}
+            )
+            .then(function (response) {
+              props.setToken(response.data.token);
+            })
+            .catch(function (error) {
+              console.log(error);
+            });
 
-        setEmail("");
-        setPassword("");
-        navigate("/todos");
-      }}
-    >
-      <label className="label">
-        Email:
-        <input
-          className="input"
-          type="email"
-          onChange={(e) => setEmail(e.target.value)}
-          value={email}
-        />
-      </label>
-      <label className="label">
-        Password:
-        <input
-          className="input"
-          type="password"
-          onChange={(e) => setPassword(e.target.value)}
-          value={password}
-        />
-      </label>
-      <input className="submit" type="submit" value="Sign In!" />
-    </form>
+          setEmail("");
+          setPassword("");
+          navigate("/todos");
+        }}
+      >
+        <label className="label">
+          Email:
+          <input
+            className="input"
+            type="email"
+            onChange={(e) => setEmail(e.target.value)}
+            value={email}
+          />
+        </label>
+        <label className="label">
+          Password:
+          <input
+            className="input"
+            type="password"
+            onChange={(e) => setPassword(e.target.value)}
+            value={password}
+          />
+        </label>
+        <input className="submit" type="submit" value="Sign In!" />
+        <p>
+          New here? <Link to="/signup">Signup!</Link>
+        </p>
+      </form>
+    </>
   );
 };
